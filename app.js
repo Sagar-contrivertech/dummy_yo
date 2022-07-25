6e5232d386d3335419b2265b3caa19140e3f20e8
Sagar Singh <sagardeveloper08@gmail.com>
2022-07-26 00:19:50 +0530

    first commit1

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'))
 port = config.port || 80
-
+const fileUpload = require('express-fileupload')
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }))
 
 const userRoutes = require('./routes/userRoutes')
 const cosumerRoutes = require('./routes/cosumerRoutes')
