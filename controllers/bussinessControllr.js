c91beac31f41c349a8474495150d37b19ab79623
Sagar Singh <sagardeveloper08@gmail.com>
2022-07-26 13:14:55 +0530

    chnage create stream

--- controllers/bussinessControllr.js
+++ controllers/bussinessControllr.js
@@ -21,7 +21,7 @@ AWS.config.credentials = {
 
 AWS.config.region = "ap-south-1"
 
-const s3 = new AWS.S3({apiVersion: '2006-03-01'})
+const s3 = new AWS.S3({ apiVersion: '2006-03-01' })
 
 exports.addbussiness = catchAsyncErrors(async (req, res) => {
     try {
@@ -63,104 +63,108 @@ exports.addbussiness = catchAsyncErrors(async (req, res) => {
         let FileNameSplit
         const newupdateobject = {}
         const fileNamevar = req.body.Images;
-        // console.log(fileNamevar, "filenamevar")
+        console.log(fileNamevar, "filenamevar")
+
 
 
         for (let i = 0; i < fileNamevar.length; i++) {
 
             if (Object.keys(fileNamevar[i])[0] === "logo") {
-                fileContent = fs.createReadStream(fileNamevar[i].logo);
+                fileContent = fs.readFileSync(fileNamevar[i].logo,'base64');
                 FileNameSplit = fileNamevar[i].logo.split("/")
             } else if (Object.keys(fileNamevar[i])[0] === "ownerImage") {
-                fileContent = fs.createReadStream(fileNamevar[i].ownerImage);
+                fileContent = fs.readFileSync(fileNamevar[i].ownerImage,'base64');
                 FileNameSplit = fileNamevar[i].ownerImage.split("/")
             } else if (Object.keys(fileNamevar[i])[0] === "bannerImage") {
-                fileContent = fs.createReadStream(fileNamevar[i].bannerImage);
+                fileContent = fs.readFileSync(fileNamevar[i].bannerImage,'base64');
                 FileNameSplit = fileNamevar[i].bannerImage.split("/")
             } else if (Object.keys(fileNamevar[i])[0] === "idproof") {
-                fileContent = fs.createReadStream(fileNamevar[i].idproof);
+                fileContent = fs.readFileSync(fileNamevar[i].idproof,'base64');
                 FileNameSplit = fileNamevar[i].idproof.split("/")
             }
-        }
-        console.log(fileContent)
-        const params = {
-            Bucket: BUCKET_NAME,
-            Key: FileNameSplit[FileNameSplit.length - 1], // File name you want to save as in S3
-            Body: fileContent,
-            ContentEncoding: 'base64',
-            ContentType: 'multipart/form-data'
-        };
-        console.log(params)
-        // Uploading files to the bucket
+            // 
 
-        s3.upload(params, async function (err, data) {
-            if (err) {
-                throw err;
-            }
-            fileNamevar && fileNamevar.map(m => {
-                if (Object.keys(m).toString() === 'logo') {
-                    newupdateobject.logo = data.Location
-                }
-                else if (Object.keys(m).toString() === 'ownerImage') {
-                    newupdateobject.ownerImage = data.Location
-                }
-                else if (Object.keys(m).toString() === 'bannerImage') {
-                    newupdateobject.bannerImage = data.Location
-                }
-                else {
-                    newupdateobject.idproof = data.Location
-                }
-                return newupdateobject
-            })
-            // console.log(newupdateobject, "169")
+            // console.log(fileContent)
+            const params = {
+                Bucket: BUCKET_NAME,
+                Key: FileNameSplit[FileNameSplit.length - 1], // File name you want to save as in S3
+                Body: fileContent,
+                ContentEncoding: 'base64',
+                ContentType: 'multipart/form-data'
+            };
+            console.log(params)
+            // Uploading files to the bucket
 
-            const createOwner = await BusinessOwner.create({
-                name: req.body.name,
-                ownerName: req.body.ownerName,
-                ownerEmail: req.body.ownerEmail,
-
-                dateOfBirth: req.body.dateOfBirth,
-                storeName: req.body.storeName,
-                bussinessName: req.body.bussinessName,
-                lName: req.body.lName,
-                bussinessEmailId: req.body.bussinessEmailId,
-                bussinessWebsite: req.body.bussinessWebsite,
-                bussinessNIF: req.body.bussinessNIF,
-                bussinessType: req.body.bussinessType,
-                bussinessServices: req.body.bussinessServices,
-                bussinessLandlineNumber: req.body.bussinessLandlineNumber,
-                bussinessMobileNumber: req.body.bussinessMobileNumber,
-                openingTime: req.body.openingTime,
-                closingTime: req.body.closingTime,
-                range: req.body.range,
-                workSince: req.body.workSince,
-                designation: req.body.designation,
-                merchant_type: req.body.merchant_type,
-                bankName: req.body.bankName,
-                bankAccountNumber: req.body.bankAccountNumber,
-                bankAccountHolderName: req.body.bankAccountHolderName,
-                bankCode: req.body.bankCode,
-                currentAddress: req.body.currentAddress,
-                // images field 
-                Images: [{
-                    bannerImage: newupdateobject.bannerImage,
-                    owneridproofurl: newupdateobject.idproof,
-                    ownerImages: newupdateobject.ownerImage,
-                    bussinessLogo: newupdateobject.logo,
+            s3.upload(params, async function (err, data) {
+                if (err) {
+                    throw err;
                 }
-                ],
-                bussinessImages: req.body.bussinessImages,
-
-                ownerImage: req.body.ownerImage
-            })
-            if (createOwner) {
-                // console.log(imageData, createOwner._id)
-                await createOwner.save()
-                res.status(200).json({
-                    success: true,
-                    message: 'create user',
-                    data: createOwner
+                fileNamevar && fileNamevar.map(m => {
+                    if (Object.keys(m).toString() === 'logo') {
+                        newupdateobject.logo = data.Location
+                    }
+                    else if (Object.keys(m).toString() === 'ownerImage') {
+                        newupdateobject.ownerImage = data.Location
+                    }
+                    else if (Object.keys(m).toString() === 'bannerImage') {
+                        newupdateobject.bannerImage = data.Location
+                    }
+                    else {
+                        newupdateobject.idproof = data.Location
+                    }
+                    return newupdateobject
                 })
+                // console.log(newupdateobject, "169")
+
+                const createOwner = await BusinessOwner.create({
+                    name: req.body.name,
+                    ownerName: req.body.ownerName,
+                    ownerEmail: req.body.ownerEmail,
+
+                    dateOfBirth: req.body.dateOfBirth,
+                    storeName: req.body.storeName,
+                    bussinessName: req.body.bussinessName,
+                    lName: req.body.lName,
+                    bussinessEmailId: req.body.bussinessEmailId,
+                    bussinessWebsite: req.body.bussinessWebsite,
+                    bussinessNIF: req.body.bussinessNIF,
+                    bussinessType: req.body.bussinessType,
+                    bussinessServices: req.body.bussinessServices,
+                    bussinessLandlineNumber: req.body.bussinessLandlineNumber,
+                    bussinessMobileNumber: req.body.bussinessMobileNumber,
+                    openingTime: req.body.openingTime,
+                    closingTime: req.body.closingTime,
+                    range: req.body.range,
+                    workSince: req.body.workSince,
+                    designation: req.body.designation,
+                    merchant_type: req.body.merchant_type,
+                    bankName: req.body.bankName,
+                    bankAccountNumber: req.body.bankAccountNumber,
+                    bankAccountHolderName: req.body.bankAccountHolderName,
+                    bankCode: req.body.bankCode,
+                    currentAddress: req.body.currentAddress,
+                    // images field 
+                    Images: [{
+                        bannerImage: newupdateobject.bannerImage,
+                        owneridproofurl: newupdateobject.idproof,
+                        ownerImages: newupdateobject.ownerImage,
+                        bussinessLogo: newupdateobject.logo,
+                    }
+                    ],
+                    bussinessImages: req.body.bussinessImages,
+
+                    ownerImage: req.body.ownerImage
+                })
+                if (createOwner) {
+                    // console.log(imageData, createOwner._id)
+                    await createOwner.save()
+                    res.status(200).json({
+                        success: true,
+                        message: 'create user',
+                        data: createOwner
+                    })
+                    return
+                }
                 if (!createOwner) {
                     res.status(400).json({
                         success: false,
@@ -168,9 +172,9 @@ exports.addbussiness = catchAsyncErrors(async (req, res) => {
                     })
                     return
                 }
+            });
+        }
 
-            }
-        });
 
     } catch (error) {
         console.log(error);
